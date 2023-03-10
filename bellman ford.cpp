d4fe4adb34fdbb5bf1583b33a1b955ab4035f43e
striker0234 <98385735+striker0234@users.noreply.github.com>
2023-03-10 11:03:25 +0530

    Add files via upload

---
+++ bellman ford.cpp
@@ -0,0 +1,94 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+#define int                          long long
+#define fo(i, n)                     for (int i = 0; i < n; i++)
+#define fo2(i, z, n)                 for (int i = z; i <= n; i++)
+#define pb                           push_back
+#define all(var)                     var.begin(), var.end()
+#define desc                         greater<int>()
+#define set_bits                     __builtin_popcountll
+#define YES                          cout << "YES\n";
+#define NO                           cout << "NO\n";
+#define Yes                          cout << "Yes\n";
+#define No                           cout << "No\n";
+#define sz                           size()
+#define sqrt(x)                      sqrtl(x)
+#define f                            first
+#define ss                           second
+#define ndl                          cout << "\n";
+typedef vector<pair<int, int>>       vpi;
+typedef vector<int>                  vi;
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds; // *s.find_by_order(), order_of_key()
+// order_of_key (val): returns the no. of values less than val
+// find_by_order (k): returns the kth largest element.(0-based)
+ 
+ 
+/*-----------------------------------------------------*/
+int M=1e9+7;
+int po(int a, int b, int mod) {int res = 1; while (b > 0) {if (b & 1)res = (res * a) % mod; a = (a * a) % mod; b = b >> 1;} return res;}
+int gcd(int a, int b) {if (b > a) {return gcd(b, a);} if (b == 0) {return a;} return gcd(b, a % b);}
+int inv(int x){ return po(x,M-2,M);}
+int add(int a, int b, int m) {a = a % m; b = b % m; return (((a + b) % m) + m) % m;}
+int mult(int a, int b, int m) {a = a % m; b = b % m; return (((a * b) % m) + m) % m;}
+int sub(int a, int b, int m) {a = a % m; b = b % m; return (((a - b) % m) + m) % m;}
+int div1(int x, int y){ return mult(x,inv(y),M); }
+/*-----------------------------------------------------*/
+
+//detect a cycle in a graph and for storing all cycles use gfg 
+
+void solve(){
+    int n,m;
+    cin>>n>>m;
+    vector<vi>adj;
+    fo(i,m){
+        int u,v,w;
+        cin>>u>>v>>w;
+        adj.pb({u,v,w});
+    }
+    vi vis(n+1);
+    //bellman-ford
+    int dis[n+1], par[n+1]={0};
+    fo2(i,1,n) dis[i]=1e9;
+    dis[1]=0; 
+    int f;
+    fo2(i,1,n){
+        f=0;
+        for (auto it: adj){
+            if (dis[it[0]]+it[2]<dis[it[1]]){
+                dis[it[1]] = dis[it[0]]+it[2];
+                par[it[1]] = it[0];
+                f=it[1];
+            }
+        }
+    }
+    if(f==0){
+        NO return;
+    }
+    YES;
+    vi cyc;
+    
+    fo2(i,1,n) f=par[f];
+    int j=f;
+    while(true){
+        cyc.pb(f);
+        f=par[f];
+        if(f==j && cyc.sz>1) break;
+    }
+    reverse(all(cyc));
+    for(auto it: cyc) cout<<it<<" ";
+    cout<<cyc[0];
+    ndl
+}
+
+int32_t main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    //int T; cin >> T; while (T--)
+    {
+        solve();
+    }
+}
